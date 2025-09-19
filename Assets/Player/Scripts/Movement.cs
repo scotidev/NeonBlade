c4417d64ef208cfa3c99fadf76061c11b229d7ff
gabrielscoti42 <gabrielscotio@gmail.com>
2025-09-19 09:27:46 -0300

    Prototype assets and Walking animation

--- Assets/Player/Scripts/Movement.cs
+++ Assets/Player/Scripts/Movement.cs
@@ -4,11 +4,13 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator playerAnimator;
 
     float directionMove = 0.0f;
+    bool isFacingRight = true;
 
     [Header("Movement Settings")]
-    [SerializeField] float moveSpeed = 2.0f;
+    [SerializeField] float moveSpeed = 4.0f;
 
     private GameInputActions playerControls;
     private InputAction move;
@@ -20,6 +22,7 @@ public class Movement : MonoBehaviour
 
     private void Start()
     {
+        playerAnimator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -37,6 +40,18 @@ public class Movement : MonoBehaviour
     void Update()
     {
         directionMove = move.ReadValue<float>();
+        FlipPlayer();
+    }
+
+    private void FlipPlayer()
+    {
+        if ((isFacingRight && directionMove < 0f) || (!isFacingRight && directionMove > 0f))
+        {
+            isFacingRight = !isFacingRight;
+            GetComponent<SpriteRenderer>().flipX = !isFacingRight;
+        }
+
+        playerAnimator.SetBool("isWalking", directionMove != 0);
     }
 
     void FixedUpdate()
